6a0d189a2f54a92752b46208a4e8e3790d4d7d51
wurongquan <408799816@qq.com>
2025-05-11 15:33:57 +0800

    Untrack app/build directory

--- app/build/generated/hilt/component_trees/release/dagger/hilt/internal/processedrootsentinel/codegen/_com_insnaejack_pdfgenerator_PdfGeneratorApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.insnaejack.pdfgenerator.PdfGeneratorApp"
-)
-public final class _com_insnaejack_pdfgenerator_PdfGeneratorApp {
-}
